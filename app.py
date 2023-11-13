c07530cfaeaa8ce5f7120e0861bdec167f125579
Eetu Huttula <100779993+EetuHuttula@users.noreply.github.com>
2023-11-13 15:28:05 +0200

    Created necessary pages for the app and made changes to app to get them to work

--- app.py
+++ app.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template('frontpage.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/profile")
+def profile():
+    return render_template('profile.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
