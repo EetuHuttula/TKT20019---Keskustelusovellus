deec0f7b24f2cc0fc24615d35db6ec2c9ad75e52
Eetu Huttula <100779993+EetuHuttula@users.noreply.github.com>
2023-11-16 13:02:15 +0200

    Added postresql database to the project. User can now see threads, and create new ones.

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text 
+from os import getenv
+
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
 @app.route("/")
 def index():
-    return render_template('frontpage.html')
+    query = text("SELECT * FROM messages")
+    result = db.session.execute(query)
+    messages = result.fetchall()
+    return render_template('frontpage.html', messages=messages)
+
+@app.route("/newthread")
+def newthread():
+    return render_template('newthread.html')
+
+
+@app.route("/send", methods=["POST"])
+def send():
+    title = request.form["title"]
+    content = request.form["content"]
+    sql = text("INSERT INTO messages (title, content) VALUES (:title, :content)")
+    db.session.execute(sql, {"title": title, "content": content})
+    db.session.commit()
+    return redirect("/")
+
 
 @app.route("/about")
 def about():
