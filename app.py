da3418ccac6faa1a0ffddae3f5876b8ad479d127
Eetu Huttula <100779993+EetuHuttula@users.noreply.github.com>
2023-11-17 01:26:02 +0200

    User can now register to the app, login and send messages as logged in profile. Also updates the database table "messages" with new column "user_username"

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ from flask import Flask, redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text 
 from os import getenv
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.exc import SQLAlchemyError
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-
+app.config["SECRET_KEY"] = getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
@@ -27,9 +28,17 @@ def newthread():
 def send():
     title = request.form["title"]
     content = request.form["content"]
-    sql = text("INSERT INTO messages (title, content) VALUES (:title, :content)")
-    db.session.execute(sql, {"title": title, "content": content})
+    username = session.get("username")
+      # Check if the user is logged in
+    if not username:
+        # Redirect to the login page or handle the case where the user is not logged in
+        return redirect("/login")
+
+    # Insert the message into the database with the associated username
+    sql = text("INSERT INTO messages (title, content, user_username) VALUES (:title, :content, :user_username)")
+    db.session.execute(sql, {"title": title, "content": content, "user_username": username})
     db.session.commit()
+
     return redirect("/")
 
 
@@ -41,13 +50,78 @@ def about():
 def profile():
     return render_template('profile.html')
 
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    username = request.form["username"]
-    password = request.form["password"]
+    if request.method == "POST":
+        # Handle the login logic for POST requests
+        username = request.form["username"]
+        password = request.form["password"]
 
-    session["username"] = username
-    return render_template('login.html')
+        # Validate username and password
+        try:
+            query = text("SELECT id, password FROM users WHERE username=:username")
+            result = db.session.execute(query, {"username": username})
+            user = result.fetchone()
+
+            if not user:
+                # TODO: Handle invalid username
+                return redirect("/")
+
+            stored_hashed_password = user.password
+
+            if check_password_hash(stored_hashed_password, password):
+                # Password is correct
+                session["username"] = username
+                return redirect("/")
+            else:
+                # Invalid password
+                return redirect("/")
+
+        except SQLAlchemyError as e:
+            # Handle the database error, log it, or provide a user-friendly message
+            db.session.rollback()
+            print(f"Database Error: {str(e)}")
+            return redirect("/")
+        finally:
+            db.session.close()    
+    else:
+        # Handle the rendering of the login form for GET requests
+        return render_template('login.html')
+    
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+
+        # Tallenna käyttäjä tietokantaan
+        try:
+            sql = text("INSERT INTO users (username, password) VALUES (:username, :password)")
+            db.session.execute(sql, {"username": username, "password": hashed_password})
+            db.session.commit()
+
+            session["username"] = username  # Kirjaudu sisään automaattisesti
+            return redirect("/")
+
+        except Exception as e:
+            db.session.rollback()
+            print(f"Registration Error: {str(e)}")
+            return render_template('registration_failed.html')
+
+        finally:
+            db.session.close()
+
+    else:
+        return render_template('register.html')
+
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect("/")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- schematic.sql
+++ schematic.sql
@@ -1,8 +1,18 @@
-CREATE TABLE messages (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    content TEXT NOT NULL,
-    title VARCHAR(255)
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password TEXT NOT NULL
 );
 
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    user_username VARCHAR(255) REFERENCES users(username) NOT NULL
+);
+
+ALTER TABLE messages
+ALTER COLUMN user_id TYPE VARCHAR(255);
+
+
 
