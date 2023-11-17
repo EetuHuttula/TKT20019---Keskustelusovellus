7f087a5c5653bbd0fb4be4759cc7093f0085830c
Eetu Huttula <100779993+EetuHuttula@users.noreply.github.com>
2023-11-17 13:46:32 +0200

    made profile page to show some data from the user like creation date, name, threads created, postst made, updated database to show right time format, updated app.py to support changes to profile page. linked right css file to every page and added "fronpage" link to navbar

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, send_file
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text 
 from os import getenv
@@ -46,9 +46,49 @@ def send():
 def about():
     return render_template('about.html')
 
+
 @app.route("/profile")
 def profile():
-    return render_template('profile.html')
+    if 'username' not in session:
+        return redirect('/login')  # Redirect to login if not logged in
+
+    username = session['username']
+    user = get_user_info(username)
+
+    if user:
+        # Fetch thread count for the user
+        thread_count = get_thread_count(user.username)
+
+        # Fetch post count for the user
+        post_count = get_post_count(user.username)
+
+        return render_template('profile.html', thread_count=thread_count, post_count=post_count, user=user)
+    else:
+        # Handle the case when the user is not found
+        return render_template('profile.html', thread_count=0, post_count=0, user=None)
+
+# Function to retrieve user information from the database
+def get_user_info(username):
+    query = text("SELECT * FROM users WHERE username=:username")
+    result = db.session.execute(query, {"username": username})
+    user = result.fetchone()
+    return user
+
+# Function to get thread count for a user
+def get_thread_count(username):
+    query = text("SELECT COUNT(*) FROM threads WHERE user_username=:username")
+    result = db.session.execute(query, {"username": username})
+    thread_count = result.scalar()  # scalar() fetches the count value
+    return thread_count
+
+# Function to get post count for a user
+def get_post_count(username):
+    query = text("SELECT COUNT(*) FROM posts WHERE user_username=:username")
+    result = db.session.execute(query, {"username": username})
+    post_count = result.scalar()  # scalar() fetches the count value
+    return post_count
+
+#LOGIN function
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -87,7 +127,9 @@ def login():
     else:
         # Handle the rendering of the login form for GET requests
         return render_template('login.html')
-    
+
+#REGISTER function
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -116,6 +158,7 @@ def register():
     else:
         return render_template('register.html')
 
+#LOGOUT function
 
 @app.route("/logout")
 def logout():
--- schematic.sql
+++ schematic.sql
@@ -1,21 +1,23 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    creation_date VARCHAR(16) DEFAULT to_char(CURRENT_TIMESTAMP, 'DD.MM.YY HH24:MI')
 );
 
 CREATE TABLE threads (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
-    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    user_username VARCHAR(255) REFERENCES users(username) NOT NULL
+    creation_date VARCHAR(16) DEFAULT to_char(CURRENT_TIMESTAMP, 'DD.MM.YY HH24:MI'),
+    user_username VARCHAR(255) REFERENCES users(username) ON DELETE CASCADE NOT NULL
 );
+
 
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
     content TEXT NOT NULL,
-    post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    post_date VARCHAR(16) DEFAULT to_char(CURRENT_TIMESTAMP, 'DD.MM.YY HH24:MI'),
     user_username VARCHAR(255) REFERENCES users (username) NOT NULL,
     thread_id INTEGER REFERENCES threads (id) NOT NULL
 );
