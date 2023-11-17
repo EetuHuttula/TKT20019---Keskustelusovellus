7d8d891c37294e19ba3c6b91237f4bcaf6d7159e
Eetu Huttula <100779993+EetuHuttula@users.noreply.github.com>
2023-11-18 00:48:28 +0200

    User can now reply to a specific threads.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, session, send_file
+from flask import Flask, redirect, render_template, request, session, send_file, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text 
 from os import getenv
@@ -19,10 +19,54 @@ def index():
     threads = result.fetchall()
     return render_template('frontpage.html', threads=threads)
 
-@app.route("/newthread")
+
+@app.route("/newthread", methods=["GET", "POST"])
 def newthread():
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        username = session.get("username")
+
+        if not username:
+            return redirect(url_for("login"))
+
+        thread = thread(title=title, content=content, user_username=username)
+        db.session.add(thread)
+        db.session.commit()
+
+        return redirect(url_for("index"))
+
     return render_template('newthread.html')
 
+@app.route('/thread/<int:thread_id>', methods=['GET', 'POST'])
+def view_thread(thread_id):
+    if request.method == 'GET':
+        # Fetch the thread and its posts using direct SQL queries
+        query_thread = text("SELECT * FROM threads WHERE id = :thread_id")
+        result_thread = db.session.execute(query_thread, {"thread_id": thread_id})
+        selected_thread = result_thread.fetchone()
+
+        query_posts = text("SELECT * FROM posts WHERE thread_id = :thread_id")
+        result_posts = db.session.execute(query_posts, {"thread_id": thread_id})
+        posts = result_posts.fetchall()
+
+        return render_template('view_thread.html', thread=selected_thread, posts=posts)
+
+    elif request.method == 'POST':
+        # Handle the form submission for posting a reply
+        content = request.form['content']
+        username = session.get('username')
+
+        if not username:
+            # Redirect to login or handle the case where the user is not logged in
+            return redirect('/login')
+
+        # Insert the reply into the posts table using direct SQL query
+        query_insert_post = text("INSERT INTO posts (content, user_username, thread_id) VALUES (:content, :username, :thread_id)")
+        db.session.execute(query_insert_post, {"content": content, "username": username, "thread_id": thread_id})
+        db.session.commit()
+
+        return redirect(url_for('view_thread', thread_id=thread_id))
 
 @app.route("/send", methods=["POST"])
 def send():
