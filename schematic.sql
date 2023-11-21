9190066583830389d68cb2ef18b322e1e396c051
Eetu Huttula <100779993+EetuHuttula@users.noreply.github.com>
2023-11-21 10:56:57 +0200

    Time format changes

--- schematic.sql
+++ schematic.sql
@@ -2,14 +2,14 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    creation_date VARCHAR(16) DEFAULT to_char(CURRENT_TIMESTAMP, 'DD.MM.YY HH24:MI')
+    creation_date VARCHAR(16) DEFAULT to_char(CURRENT_TIMESTAMP, 'DD.MM.YY')
 );
 
 CREATE TABLE threads (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
-    creation_date VARCHAR(16) DEFAULT to_char(CURRENT_TIMESTAMP, 'DD.MM.YY HH24:MI'),
+    creation_date VARCHAR(16) DEFAULT to_char(CURRENT_TIMESTAMP, 'DD.MM.YY'),
     user_username VARCHAR(255) REFERENCES users(username) ON DELETE CASCADE NOT NULL
 );
 
@@ -17,7 +17,7 @@ CREATE TABLE threads (
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
     content TEXT NOT NULL,
-    post_date VARCHAR(16) DEFAULT to_char(CURRENT_TIMESTAMP, 'DD.MM.YY HH24:MI'),
+    post_date VARCHAR(16) DEFAULT to_char(CURRENT_TIMESTAMP, 'DD.MM.YY'),
     user_username VARCHAR(255) REFERENCES users (username) NOT NULL,
     thread_id INTEGER REFERENCES threads (id) NOT NULL
 );
