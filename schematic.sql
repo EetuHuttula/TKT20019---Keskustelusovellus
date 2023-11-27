b12ab2c710c8d3521a70eb73b39ad42b8d630a79
Eetu Huttula <100779993+EetuHuttula@users.noreply.github.com>
2023-11-27 18:02:07 +0200

    database has 5 tables now

--- schematic.sql
+++ schematic.sql
@@ -22,5 +22,19 @@ CREATE TABLE posts (
     thread_id INTEGER REFERENCES threads (id) NOT NULL
 );
 
+CREATE TABLE polls (
+    id SERIAL PRIMARY KEY,
+    question TEXT NOT NULL,
+    creation_date VARCHAR(16) DEFAULT to_char(CURRENT_TIMESTAMP, 'DD.MM.YY'),
+    user_username VARCHAR(255) REFERENCES users (username) NOT NULL
+);
+
+CREATE TABLE options (
+    id SERIAL PRIMARY KEY,
+    poll_id INTEGER REFERENCES polls (id) ON DELETE CASCADE NOT NULL,
+    option_text VARCHAR(255) NOT NULL,
+    vote_count INTEGER DEFAULT 0
+);
+
 
 
