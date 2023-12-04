8cf2a3ec4006592803fcfab5b7b66a5a45ef52ed
Eetu Huttula <100779993+EetuHuttula@users.noreply.github.com>
2023-12-04 18:05:00 +0200

    added thread like count

--- prof.py
+++ prof.py
@@ -20,11 +20,14 @@ def profile():
         # Fetch post count for the user
         post_count = get_post_count(user.username)
 
+        #Fetch like count for the user
+        like_count = get_like_count(user.username)
+
         return render_template('profile.html',
             thread_count=thread_count,
-            post_count=post_count, user=user)
+            post_count=post_count, user=user, like_count=like_count)
     return render_template('profile.html',
-        thread_count=0, post_count=0, user=None)
+        thread_count=0, post_count=0, user=None, like_count=0)
 
 # Function to retrieve user information from the database
 def get_user_info(username):
@@ -46,3 +49,9 @@ def get_post_count(username):
     result = db.session.execute(query, {"username": username})
     post_count = result.scalar()  # scalar() fetches the count value
     return post_count
+
+def get_like_count(username):
+    query = text("SELECT COUNT(*) FROM likes WHERE user_username=:username")
+    result = db.session.execute(query, {"username": username})
+    like_count = result.scalar()
+    return like_count
