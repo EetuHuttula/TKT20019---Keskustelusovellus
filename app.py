ab9a9bac7fa26de6d87193625afa303731499d63
Eetu Huttula <100779993+EetuHuttula@users.noreply.github.com>
2023-12-10 20:24:06 +0200

    Linked register page

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = getenv("SECRET_KEY")
 app.jinja_env.globals['csrf_token'] = generate_csrf_token
 #importing modules
-from src.auth import user_login
+from src.auth import user_login, user_register
 from src.threads import thread_view, thread_create_reply, thread_management, thread_like 
 from src.polls import polls_create, polls_delete, polls_view_answer
 from src.profile import profile_routes
